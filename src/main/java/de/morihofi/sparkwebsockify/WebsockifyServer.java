d06a802323067b1e4c425050e987fe4b0d0247eb
Moritz Hofmann <info@morihofi.de>
2023-05-26 18:27:18 +0200

    feat: Multiple connections at the same time, now uses ByteBuffer instead of byte array

--- src/main/java/de/morihofi/sparkwebsockify/WebsockifyServer.java
+++ src/main/java/de/morihofi/sparkwebsockify/WebsockifyServer.java
@@ -2,7 +2,11 @@ package de.morihofi.sparkwebsockify;
 
 
 import org.java_websocket.WebSocket;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.exceptions.InvalidDataException;
+import org.java_websocket.framing.CloseFrame;
 import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.handshake.ServerHandshakeBuilder;
 import org.java_websocket.server.WebSocketServer;
 
 import java.io.IOException;
@@ -12,18 +16,24 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 
 public class WebsockifyServer extends WebSocketServer {
 
-    public WebsockifyServer(InetSocketAddress address) {
+    public WebsockifyServer(InetSocketAddress address, String tcpHost, int tcpPort) {
         super(address);
+        this.tcpHost = tcpHost;
+        this.tcpPort = tcpPort;
     }
 
-    Socket vncSocket;
-    WebSocket wsConn;
+    private String tcpHost;
+    private int tcpPort;
 
-    OutputStream os;
-    InputStream is;
+
+
+    HashMap<WebSocket,Socket> connections = new HashMap<>();
+
+
 
     @Override
     public void onOpen(WebSocket conn, ClientHandshake handshake) {
@@ -31,36 +41,59 @@ public class WebsockifyServer extends WebSocketServer {
         //broadcast("new connection: " + handshake.getResourceDescriptor()); //This method sends a message to all clients connected
         System.out.println("new connection to " + conn.getRemoteSocketAddress());
 
-        // TCP-Verbindung zum VNC-Server herstellen
         try {
-            vncSocket = new Socket("127.0.0.1", 5980);
-            os = vncSocket.getOutputStream();
-            is = vncSocket.getInputStream();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            //String getParams = handshake.getResourceDescriptor().split("\\?")[1]; // GET-Parameter abrufen
+            //System.out.println("GET-Parameter: " + getParams);
+
+
+            // Überprüfe den Wert des "token" GET-Parameters
+            //String tokenValue = getParameterValue(getParams, "token");
+            //if (!"Test".equals(tokenValue)) {
+            //    conn.close();
+            //    return;
+            //}
+
+            // TCP-Verbindung zum VNC-Server herstellen
+            Socket tcpSocket = new Socket(tcpHost, tcpPort);
+
+            // WebSocket-Session und TCP-Socket miteinander verbinden
+            connections.put(conn,tcpSocket);
+
+            // Starte den Thread zum Lesen der Daten vom VNC-Server und Senden an den WebSocket-Client
+            new Thread(() -> forwardDataFromVncToWebSocket(conn,tcpSocket)).start();
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+            conn.close();
         }
 
-        // WebSocket-Session und TCP-Socket miteinander verbinden
-        wsConn = conn;
 
-        // Starte den Thread zum Lesen der Daten vom VNC-Server und Senden an den WebSocket-Client
-        new Thread(this::forwardDataFromVncToWebSocket).start();
-
-        // Starte den Thread zum Lesen der Daten vom WebSocket-Client und Senden an den VNC-Server
-        //new Thread(this::forwardDataFromWebSocketToVnc).start();
 
     }
 
     @Override
     public void onClose(WebSocket conn, int code, String reason, boolean remote) {
         System.out.println("closed " + conn.getRemoteSocketAddress() + " with exit code " + code + " additional info: " + reason);
+
+        Socket tcpSocket = connections.get(conn);
+        if(!tcpSocket.isClosed()){
+            try {
+                tcpSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        connections.remove(conn);
     }
 
     @Override
     public void onMessage(WebSocket conn, String message) {
-        System.out.println("received message from " + conn.getRemoteSocketAddress() + ": " + message);
+        //System.out.println("received message from " + conn.getRemoteSocketAddress() + ": " + message);
 
         try {
+            Socket tcpSocket = connections.get(conn);
+
+            OutputStream os = tcpSocket.getOutputStream();
             os.write(message.getBytes(StandardCharsets.UTF_8));
             os.flush();
 
@@ -72,9 +105,14 @@ public class WebsockifyServer extends WebSocketServer {
 
     @Override
     public void onMessage(WebSocket conn, ByteBuffer message) {
-        System.out.println("received ByteBuffer from " + conn.getRemoteSocketAddress());
+        //System.out.println("received ByteBuffer from " + conn.getRemoteSocketAddress());
+
 
         try {
+            Socket tcpSocket = connections.get(conn);
+
+            OutputStream os = tcpSocket.getOutputStream();
+
             byte[] data = new byte[message.remaining()];
             message.get(data);
             os.write(data);
@@ -95,43 +133,58 @@ public class WebsockifyServer extends WebSocketServer {
         System.out.println("server started successfully");
     }
 
-    private void forwardDataFromVncToWebSocket() {
+   // @Override
+   // public ServerHandshakeBuilder onWebsocketHandshakeReceivedAsServer(WebSocket conn, Draft draft,
+   //                                                                    ClientHandshake request) throws InvalidDataException {
+   //     ServerHandshakeBuilder builder = super.onWebsocketHandshakeReceivedAsServer(conn, draft, request);
+   //     //In this example we don't allow any resource descriptor ( "ws://localhost:8887/?roomid=1 will be rejected but ws://localhost:8887 is fine)
+   //     if (!request.getResourceDescriptor().equals("/")) {
+   //         throw new InvalidDataException(CloseFrame.POLICY_VALIDATION, "Not accepted!");
+   //     }
+   //
+   //     return builder;
+   // }
+
+    private void forwardDataFromVncToWebSocket(WebSocket wsConn, Socket tcpSocket) {
+
+
         try {
+
 
             //TODO: ByteBuffer verwenden
             byte[] buffer = new byte[4096];
             int bytesRead;
+            InputStream is = tcpSocket.getInputStream();
             while ((bytesRead = is.read(buffer)) != -1) {
-                byte[] data = new byte[bytesRead];
-                System.arraycopy(buffer, 0, data, 0, bytesRead);
-                wsConn.send(data);
+               //byte[] data = new byte[bytesRead];
+               //System.arraycopy(buffer, 0, data, 0, bytesRead);
+                // wsConn.send(data);
+                ByteBuffer byteBuffer = ByteBuffer.wrap(buffer, 0, bytesRead);
+                wsConn.send(byteBuffer);
             }
 
 
         } catch (IOException e) {
-            e.printStackTrace();
         }
     }
+    private String getParameterValue(String queryString, String parameterName) {
+        String[] parameters = queryString.split("&");
+        for (String parameter : parameters) {
+            String[] keyValue = parameter.split("=");
+            if (keyValue.length == 2 && keyValue[0].equals(parameterName)) {
+                return keyValue[1];
+            }
+        }
+        return null;
+    }
 
-  // private void forwardDataFromWebSocketToVnc() {
-  //     try {
-  //         byte[] buffer = new byte[1024];
-  //         int bytesRead;
-
-  //         while ((bytesRead = websocketSession.getRemote().getInputStream().read(buffer)) != -1) {
-  //             vncSocket.getOutputStream().write(buffer, 0, bytesRead);
-  //         }
-  //     } catch (IOException e) {
-  //         e.printStackTrace();
-  //     }
-  // }
 
 
     public static void main(String[] args) {
         String wsHost = "localhost";
         int wsPort = 8090;
 
-        WebSocketServer server = new WebsockifyServer(new InetSocketAddress(wsHost, wsPort));
+        WebSocketServer server = new WebsockifyServer(new InetSocketAddress(wsHost, wsPort), "localhost", 5901);
         server.run();
     }
 }
